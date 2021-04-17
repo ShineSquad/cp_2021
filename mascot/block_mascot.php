2530914066b30fbe107aec3046e939136d0417e3
Rick <nikitalternative95@gmail.com>
2021-04-17 13:45:06 +0500

    succ

--- mascot/block_mascot.php
+++ mascot/block_mascot.php
@@ -1,22 +1,96 @@
 <?php
-	class block_simplehtml extends block_base {
-		function init() {
-			$this->title   = get_string('maskot', 'block_maskot');
-    		$this->version = 2021041700;
-		}
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-		function get_content() {
-			if ($this->content !== NULL) {
-				return $this->content;
-			}
+/**
+ * Newblock block caps.
+ *
+ * @package    block_mascot
+ * @copyright  Daniel Neis <danielneis@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
-			$this->content         =  new stdClass;
-			$this->content->text   = 'Здороваемся с Миром :)';
-			$this->content->footer = 'Завершающий вывод...';
+defined('MOODLE_INTERNAL') || die();
 
-			return $this->content;
-		}
-	}
+class block_mascot extends block_base {
 
-	// hi!
-?>
+    function init() {
+        $this->title = get_string('pluginname', 'block_mascot');
+    }
+
+    function get_content() {
+        global $CFG, $OUTPUT;
+
+        if ($this->content !== null) {
+            return $this->content;
+        }
+
+        if (empty($this->instance)) {
+            $this->content = '';
+            return $this->content;
+        }
+
+        $this->content = new stdClass();
+        $this->content->items = array();
+        $this->content->icons = array();
+        $this->content->footer = '';
+
+        // user/index.php expect course context, so get one if page has module context.
+        $currentcontext = $this->page->context->get_course_context(false);
+
+        if (! empty($this->config->text)) {
+            $this->content->text = $this->config->text;
+        }
+
+        $this->content = '';
+        if (empty($currentcontext)) {
+            return $this->content;
+        }
+        if ($this->page->course->id == SITEID) {
+            $this->content->text .= "site context";
+        }
+
+        if (! empty($this->config->text)) {
+            $this->content->text .= $this->config->text;
+        }
+
+        return $this->content;
+    }
+
+    // my moodle can only have SITEID and it's redundant here, so take it away
+    public function applicable_formats() {
+        return array('all' => false,
+                     'site' => true,
+                     'site-index' => true,
+                     'course-view' => true, 
+                     'course-view-social' => false,
+                     'mod' => true, 
+                     'mod-quiz' => false);
+    }
+
+    public function instance_allow_multiple() {
+          return true;
+    }
+
+    function has_config() {return true;}
+
+    public function cron() {
+            mtrace( "Hey, my cron script is running" );
+             
+                 // do something
+                  
+                      return true;
+    }
+}
